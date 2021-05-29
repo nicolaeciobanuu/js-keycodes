b31a3d8e52daf6f7d55352670018b79999353258
Nicolae Ciobanu <cnicolae2000@gmail.com>
2021-05-29 10:23:43 +0300

    :lipstick: Style all the components

---
+++ src/styled/style.tsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const CodeDisplay = styled.div`
+    font-size: 12rem;
+    display: flex;
+    justify-content: center;
+    font-family: 'Mate SC', serif;
+    margin: 0.5em 0 2em 0;
+`
+export const KeyDisplay = styled.div`
+    display: flex;
+    justify-content: center;
+`
+export const InitialMessage = styled.div`
+    font-size: 2em;
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    margin-top: 9em;
+`
+export const App = styled.div`
+`
+export const Keyboard = styled.kbd`
+    background-color: #f4f4f4;
+    border: 1px solid #ccc;
+    border-radius: 3px;
+    box-shadow: 0 1px 0 rgba(0,0,0,.1), inset 0 0 0 1px #fff;
+    color: #333;
+    font-size: 2.5rem;
+    padding: .25em .7em;
+    margin: 0.5em .15em;
+`
