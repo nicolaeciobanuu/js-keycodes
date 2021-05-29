9766574788fcb87503c39e6d2e6206c360d3af13
Nicolae Ciobanu <cnicolae2000@gmail.com>
2021-05-29 10:24:17 +0300

    :rocket: Deploy app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import * as React from 'react';
-
+import  React, {useEffect, useState} from 'react';
+import { CodeDisplay, KeyDisplay, InitialMessage} from "./components";
+import { SC } from "./styled";
 export const App = () => {
   document.title = "JS-KeyCodes";
-  
+  const [keyboardKey, setKeyboardKey] = useState("");
+  const [code, setCode] = useState(0);
+  const audio = new Audio("key.mp3");
+
+  useEffect(() => {
+    document.addEventListener("keyup", handleKey);
+    return () => {
+      document.removeEventListener("keyup", handleKey)
+    }
+  }, [])
+  const handleKey = (event: any): any => {
+    setKeyboardKey(event.key);
+    setCode(event.keyCode);
+  }
   return (
-    <div>
-      App here
-    </div>
+    <SC.App>
+      {!code && <InitialMessage/>}
+      <CodeDisplay code={code} />
+      <KeyDisplay keyboardKey={keyboardKey===" "? "Space" : keyboardKey}/>
+    </SC.App>
   )
 }
     
